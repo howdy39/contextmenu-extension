6b3599b1aec86aca91e918d0fae5fb2f2871015b
howdy39 <tatsuya.howdy@gmail.com>
2017-09-24 22:31:51 +0900

    サンプルのコードを追加

---
+++ background.js
@@ -0,0 +1,30 @@
+console.log('background.js');
+
+chrome.contextMenus.create({
+    title: "このページのドメイン名を whois で検索",
+    contexts: ["selection"],
+    onclick: function(info, tab) {
+        var url = info.pageUrl;
+        var host = url.replace(/https?:\/\/([^\/]+).*/, "$1");
+        var tree = host.split(".");
+        var parts = tree.length;
+
+        var domain;
+        if (parts == 1) {
+            alert("ドメイン名を取得できません。");
+            return;
+        } else if (parts == 2) { 
+            domain = host;
+        } else if (tree[parts - 1].length >= 3) {
+            domain = tree[parts - 2] + '.' + tree[parts - 1];
+        } else if (tree[parts - 2].length == 2) {
+            domain = tree[parts - 3] + '.' + tree[parts - 2] + '.' + tree[parts - 1];
+        } else {
+            domain = tree[parts - 2] + '.' + tree[parts - 1];;
+        }
+
+        chrome.tabs.create({
+            url: "http://akagi.jp/whois/?#!/key=" + domain
+        });
+    }
+});
